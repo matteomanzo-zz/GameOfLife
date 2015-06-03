2b48556bba649222a49577c8d194129b44a7823d
Matteo Manzo <imatteo.manzo@gmail.com>
2015-06-03 16:23:55 +0100

    moved CheckCellStatus from ecosystem to helper

--- Game/EcoSystem.cs
+++ Game/EcoSystem.cs
@@ -13,13 +13,6 @@ namespace GameOfLife
 			grid = a_grid;
 		}
 
-		public string CheckCellStatus(int xcoord, int ycoord)
-		{
-			cell = grid.QueryContents(xcoord, ycoord);
-			String status = cell.isAlive() ? "Alive" : "Dead";
-			return status;
-		}
-
 		public void PlaceLivingCell (int x, int y)
 		{
 			cell = grid.QueryContents(x, y);
@@ -110,5 +103,3 @@ namespace GameOfLife
 
 	}		
 }
-
-
--- GameTest/EcoSystemTest.cs
+++ GameTest/EcoSystemTest.cs
@@ -8,24 +8,35 @@ namespace GameOfLifeTest
 	public class EcoSystemTest
 	{
 		EcoSystem ecosystem;
+		Grid<Cell> my_grid;
 
 		[SetUp]
 		public void Init ()
 		{
-			ecosystem = new EcoSystem(new Grid<Cell>(3, 3));
+			my_grid = new Grid<Cell>(3, 3);
+			ecosystem = new EcoSystem(my_grid);
+
+
+		}
+
+		private string CheckCellStatus(int xcoord, int ycoord)
+		{
+			Cell this_cell = my_grid.QueryContents(xcoord, ycoord);
+			String status = this_cell.isAlive() ? "Alive" : "Dead";
+			return status;
 		}
 
 		[Test]
 		public void CanCheckCellStatus()
 		{
-			Assert.AreEqual("Dead", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual("Dead", CheckCellStatus(1, 1));
 		}
 
 		[Test]
 		public void CanPlaceLivingCells()
 		{
 			ecosystem.PlaceLivingCell(1, 1);
-			Assert.AreEqual ("Alive", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Alive", CheckCellStatus(1, 1));
 		}
 
 		[Test]
@@ -33,9 +44,9 @@ namespace GameOfLifeTest
 		{
 			ecosystem.PlaceLivingCell(1, 1);
 			ecosystem.PlaceLivingCell (1, 0);
-			Assert.AreEqual ("Alive", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Alive", CheckCellStatus(1, 1));
 			ecosystem.Refresh ();
-			Assert.AreEqual ("Dead", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Dead", CheckCellStatus(1, 1));
 		}
 
 		[Test]
@@ -46,9 +57,9 @@ namespace GameOfLifeTest
 			ecosystem.PlaceLivingCell(1, 0);
 			ecosystem.PlaceLivingCell (1, 2);
 			ecosystem.PlaceLivingCell (0, 2);
-			Assert.AreEqual ("Alive", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Alive", CheckCellStatus(1, 1));
 			ecosystem.Refresh ();
-			Assert.AreEqual ("Dead", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Dead", CheckCellStatus(1, 1));
 		}
 
 		[Test]
@@ -57,11 +68,11 @@ namespace GameOfLifeTest
 			ecosystem.PlaceLivingCell(0, 1);
 			ecosystem.PlaceLivingCell(1, 2);
 			ecosystem.PlaceLivingCell (1, 0);
-			Assert.AreEqual ("Dead", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Dead", CheckCellStatus(1, 1));
 			ecosystem.Refresh ();
-			Assert.AreEqual ("Alive", ecosystem.CheckCellStatus(1, 1));
+			Assert.AreEqual ("Alive", CheckCellStatus(1, 1));
 		}
+			
 	}
-
 }
 
